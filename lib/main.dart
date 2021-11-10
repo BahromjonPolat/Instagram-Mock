06cc9cd3af60324d52960d0ee53d34f6666d2d5d
BahromjonPolat <itjunior.uz@gmail.com>
2021-11-10 11:29:37 +0500

    Ilova firebase'ga ulandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mockinstagram/constants/colors.dart';
 import 'package:mockinstagram/screens/auth/login_page.dart';
 import 'package:mockinstagram/screens/home/home_page.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
